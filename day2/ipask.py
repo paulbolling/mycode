e0c2aa82f85e5eddd42fa756a124e717f1bb167a
Paul Bolling <alanpbolling@gmail.com>
2018-05-22 13:42:07 -0400

    message

---
+++ day2/ipask.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env
+#Collect IP and MAC and print to the screen.
+#by Paul Bolling
+
+print ('I need you to tell me your IP address:')
+IP = input()
+print ('You will also need to tell me your MAC address:')
+MAC = input2()
+print ('I understand that your IP address is' + IP + 'and your MAC is' + input2)
