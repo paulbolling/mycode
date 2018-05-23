0b35fdd658aabd3deec95b2a952d5c65f78566c1
Paul Bolling <alanpbolling@gmail.com>
2018-05-23 11:28:04 -0400

    message

---
+++ mix-list2/mix-list2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#Mix list 2
+
+proto = ['ssh', 'http', 'https']
+print(proto)
+print(proto[1])
+proto.append('dns') # this line will add 'dns' to the end of our list
+print(proto)
