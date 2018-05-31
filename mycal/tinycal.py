9110a1997e9469449ddb830d43ae3574fd88b39f
Paul Bolling <alanpbolling@gmail.com>
2018-05-31 16:32:26 -0400

    mesage

--- mycal/tinycal.py
+++ mycal/tinycal.py
@@ -4,5 +4,5 @@ import calendar
 
 for i in range(1,13):
     lilcal = calendar.month(2018, i)
-    print("\n\nHere is a tiny calendar:")
+    print("\n\nHere is a tiny calendar: \n")
     print(lilcal)
