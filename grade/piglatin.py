d26e1d357e8da59562816b953b30de2552f41887
Paul Bolling <alanpbolling@gmail.com>
2018-06-03 01:05:58 -0400

    project

---
+++ grade/piglatin.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#Made by Paul Bolling
+
+
+def  piglat(word):
+    first_letter = word[0]
+    
+    #check if word starts with a vowel
+    if first_letter in 'aeiou':
+        pig_word = word + 'ay'
+    else:
+        pig_word = word[1:] + first_letter + 'ay'
+        
+    return pig_word
+
+print('Enter word to translate to Pig Latin :')
+
+############working on getting to be interactive on the command line##########
+
+
