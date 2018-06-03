c64405c33d8df4d7221ae961c99706acbf54c97a
Paul Bolling <alanpbolling@gmail.com>
2018-06-03 01:54:54 -0400

    Saturday 11pm

--- grade/piglatin.py
+++ grade/piglatin.py
@@ -3,22 +3,21 @@
 #Made by Paul Bolling
 
 
-def  piglat(word):
-    first_letter = word[0]
+def  piglat(word):										#function
+    first_letter = word.lower()[0]
     
-    #check if word starts with a vowel
+    												#check if word starts with a vowel
     if first_letter in 'aeiou':
         pig_word = word + 'ay'
-    else:
+    else:											#if not a vowel add the first letter to the end and add ay
         pig_word = word[1:] + first_letter + 'ay'
         
     return pig_word
 
-print('Enter word to translate to Pig Latin :')
-word = input()
+print('Enter a word to translate to Pig Latin :')
+word = input()             									#ask for a word
+print('That is ' + piglat(word) + ' in Pig Latin') 						#output translated word
 
-print('That is ' + piglat(word) + ' in Pig Latin')
-
-############working on getting to be interactive on the command line##########
+############working on getting more words as input##########
 
 
